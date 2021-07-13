60caee3578f25f53a89e32d363791eeb5342f44b
Nobuo Saeki <saeki@booklista.co.jp>
2021-07-13 16:55:05 +0900

    Finished: Construct a screen

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,30 @@
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PureInboxScreen } from './InboxScreen';
+import * as TaskLisStories from './TaskList.stories';
+
+const store = {
+    getState: () => {
+        return {
+            tasks: TaskLisStories.Default.args.tasks,
+        };
+    },
+    subscribe: () => 0,
+    dispatch: action("dispatch"),
+};
+
+export default {
+    component: PureInboxScreen,
+    decoreators: [story => <Provider store={store}>{story()}</Provider>] ,
+    title: 'InboxScreen',
+};
+
+const Template = args => <PureInboxScreen {...args} />
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+    error: 'Something',
+};
