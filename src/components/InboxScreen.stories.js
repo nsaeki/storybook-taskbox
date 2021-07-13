8bc2d520bd7b244c3ecc57ff20e90822bf1e3f89
Nobuo Saeki <saeki@booklista.co.jp>
2021-07-13 19:23:37 +0900

    Fix error in InboxScreen storybook

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -2,25 +2,25 @@ import { action } from '@storybook/addon-actions';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PureInboxScreen } from './InboxScreen';
-import * as TaskLisStories from './TaskList.stories';
+import * as TaskListStories from './TaskList.stories';
 
 const store = {
     getState: () => {
         return {
-            tasks: TaskLisStories.Default.args.tasks,
+            tasks: TaskListStories.Default.args.tasks,
         };
     },
     subscribe: () => 0,
-    dispatch: action("dispatch"),
+    dispatch: action('dispatch'),
 };
 
 export default {
     component: PureInboxScreen,
-    decoreators: [story => <Provider store={store}>{story()}</Provider>] ,
+    decorators: [story => <Provider store={store}>{story()}</Provider>] ,
     title: 'InboxScreen',
 };
 
-const Template = args => <PureInboxScreen {...args} />
+const Template = args => <PureInboxScreen {...args} />;
 
 export const Default = Template.bind({});
 
